8a91584e87e4539ffd27be52d3e40c5de092cfe1
Diderik <diderikdm@gmail.com>
2022-12-01 09:28:29 +0100

    day 1

--- day 1 part 1 & 2.py
+++
@@ -1,4 +0,0 @@
-with open("day1.txt", "r") as file:
-    data = [[int(y) for y in x.splitlines()] for x in file.read().split("\n\n")]
-    print(max(sum(x) for x in data))
-    print(sum(sorted(sum(x) for x in data)[-3:]))
