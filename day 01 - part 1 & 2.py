7b97b03dbd5fd520dbbcfd070466d61fbc4041ef
Diderik <diderikdm@gmail.com>
2022-12-03 14:26:19 +0100

    day 1

--- day 01 - part 1 & 2.py
+++ day 01 - part 1 & 2.py
@@ -1,2 +1,5 @@
-d=sorted(sum(map(int,x.splitlines()))for x in open("d","r").read().split("\n\n"));print(d[-1],sum(d[-3:]))
-print(len("""d=sorted(sum(map(int,x.splitlines()))for x in open("d","r").read().split("\n\n"));print(d[-1],sum(d[-3:]))"""))
+with open("Advent-of-Code-2022\day 01.txt", "r") as file:
+    data = [[int(y) for y in x.splitlines()] for x in file.read().split("\n\n")]
+    sorted_data = sorted(sum(x) for x in data)
+    print(max(sorted_data))
+    print(sum(sorted_data[-3:]))
