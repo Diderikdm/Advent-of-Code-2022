30eaecf44746cd93b49449520509a467e7febde3
Diderik <diderikdm@gmail.com>
2022-12-06 08:24:33 +0100

    day 6

--- day 06 - part 1 & 2.py
+++ day 06 - part 1 & 2.py
@@ -1,5 +1,4 @@
 with open("day 06.txt", "r") as file:
     data = file.read()
-    for x in (3, 13):
-        print(next((i + 1 for i in range(x, len(data) + 1) if len(set(data[i - x : i + 1])) == x + 1)))
-    
+    for x in (4, 14):
+        print(next((i for i in range(x, len(data) + 1) if len(set(data[i - x : i])) == x)))
