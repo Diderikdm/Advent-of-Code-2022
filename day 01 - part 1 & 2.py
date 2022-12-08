ecbaae2256aa43d9278ced75cb1fce3b63aa7d1a
Diderik <diderikdm@gmail.com>
2022-12-08 14:18:32 +0100

    refactors

--- day 01 - part 1 & 2.py
+++
@@ -1,5 +0,0 @@
-with open("day 01.txt", "r") as file:
-    data = [[int(y) for y in x.splitlines()] for x in file.read().split("\n\n")]
-    sorted_data = sorted(sum(x) for x in data)
-    print(max(sorted_data))
-    print(sum(sorted_data[-3:]))
--- day 06 - part 1 & 2.py
+++
@@ -1,4 +0,0 @@
-with open("day 06.txt", "r") as file:
-    data = file.read()
-    for x in (4, 14):
-        print(next((i for i in range(x, len(data) + 1) if len(set(data[i - x : i])) == x)))
