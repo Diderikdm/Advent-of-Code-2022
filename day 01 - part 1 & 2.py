119d716c9feed2c8fc564f49d067dd72a4e35f54
Diderik <diderikdm@gmail.com>
2022-12-08 13:52:20 +0100

    day 1

--- day 01 - part 1 & 2.py
+++ day 01 - part 1 & 2.py
@@ -1,4 +1,4 @@
-with open("Advent-of-Code-2022\day 01.txt", "r") as file:
+with open("day 01.txt", "r") as file:
     data = [[int(y) for y in x.splitlines()] for x in file.read().split("\n\n")]
     sorted_data = sorted(sum(x) for x in data)
     print(max(sorted_data))
