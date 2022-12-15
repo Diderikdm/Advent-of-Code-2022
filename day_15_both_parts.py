7b8cb78bfb671ed9c4c8574d3cc9276aa79fcd3e
Diderik <diderikdm@gmail.com>
2022-12-15 13:05:53 +0100

    day 15

--- day_15_both_parts.py
+++ day_15_both_parts.py
@@ -1,7 +1,18 @@
-def find(set_y, p2=False):
+def line_intersect(Ax1, Ay1, Ax2, Ay2, Bx1, By1, Bx2, By2):
+    if (d := (By2 - By1) * (Ax2 - Ax1) - (Bx2 - Bx1) * (Ay2 - Ay1)):
+        if 0 <= (uA := ((Bx2 - Bx1) * (Ay1 - By1) - (By2 - By1) * (Ax1 - Bx1)) / d) <= 1 \
+        and 0 <= (uB := ((Ax2 - Ax1) * (Ay1 - By1) - (Ay2 - Ay1) * (Ax1 - Bx1)) / d) <= 1:
+            return int(Ax1 + uA * (Ax2 - Ax1)), int(Ay1 + uA * (Ay2 - Ay1))
+    
+def find(set_y):
     x_ranges = set()
+    m, wn, ws, ne, se = [], [], [], [], []
     for sensor, beacon in data:
         manhattan = sum(abs(a - b) for a, b in zip(sensor, beacon))
+        wn.append(((w := (sensor[0] - manhattan - 1, sensor[1])), (n := (sensor[0], sensor[1] - manhattan - 1))))
+        ws.append((w, (s := (sensor[0], sensor[1] + manhattan + 1))))
+        ne.append((n, (e := (sensor[0] + manhattan + 1, sensor[1]))))
+        se.append((s, e))
         if (man_y := abs(sensor[1] - set_y)) <= manhattan:
             x_ranges.add((sensor[0] - (man_x := manhattan - man_y), sensor[0] + man_x))
     total_range = []
@@ -16,18 +27,23 @@ def find(set_y, p2=False):
             end = y
     if (start, end) not in total_range:
         total_range.append((start, end))
-    if not p2:
-        return sum(abs(x[1] - x[0]) + 1 for x in total_range) - sum([x[1] == set_y for x in beacons])
-    elif len(total_range) > 1:
-        return (total_range[0][1] + 1) * 4000000 + set_y
-    return None
+    return m, wn, ws, ne, se, sum(abs(x[1] - x[0]) + 1 for x in total_range) - sum([x[1] == set_y for x in beacons])
         
 with open("day_15.txt", "r") as file:
     set_y = 2000000
     data = [((z := [int(x.split(" ")[y].split("=")[1].strip(",").strip(":")) for y in [2, 3, -2, -1]])[:2], z[2:]) for x in file.read().splitlines()]
     beacons = set(tuple(x[1]) for x in data)
-    p1 = find(set_y)
-    for y in range(4000000):
-        if (result := find(y, True)):
-            break
-    print("day 15: ", p1, result)
+    m, wn, ws, ne, se, p1 = find(set_y)
+    points = set()
+    p2 = None
+    while not p2:
+        for a, b in wn + se:
+            for c, d in ws + ne:
+                if (hit := line_intersect(*a, *b, *c, *d)):
+                    if 0 <= hit[0] <= 4000000 and 0 <= hit[1] <= 4000000:
+                        for e, (sensor, beacon) in enumerate(data):
+                            if sum(abs(a - b) for a, b in zip(sensor, hit)) <= sum(abs(a - b) for a, b in zip(sensor, beacon)):
+                                break
+                        else:
+                            p2 = hit[0] * 4000000 + hit[1]
+    print("day 15: ", p1, p2)
