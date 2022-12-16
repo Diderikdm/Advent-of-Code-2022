9e26ccb77a671ddc47bf20a81ea8755bb57ba3b3
Diderik <diderikdm@gmail.com>
2022-12-16 18:42:54 +0100

    day 16

--- day_16_both_parts.py
+++ day_16_both_parts.py
@@ -1,3 +1,4 @@
+
 from heapq import heapify, heappop, heappush
 
 def find_distances(start, data, interesting, queue, paths):
@@ -20,7 +21,7 @@ def path_finder(data, distances, queue, part1=False, end=None):
     while queue:
         time, release, p1, p2, seen = heappop(queue)
         if time == end + 1:
-            return -min(best.values())
+            break
         (time, valve), (time2, valve2) = (p1, p2) if part1 else sorted([p1, p2])
         for k, v in distances[valve].items():
             if k not in seen and (next_time := time + v + 1) <= end:
@@ -36,4 +37,4 @@ with open("day_16.txt", "r") as file:
     data = {(z := x.split(" "))[1] : (int(z[4].strip("rate=").strip(";")), [y.strip(",") for y in z[9:]]) for x in file.read().splitlines()}
     interesting = set([k for k, v in data.items() if v[0] > 0] + ["AA"])
     distances = {x : find_distances(x, data, interesting, [tuple([0, x])], {}) for x in interesting}
-    print("day 16: ", path_finder(data, distances, [(0, 0, (0, "AA"), (0, "AA"), ["AA"])], part1=True, end=30), path_finder(data, distances, [(0, 0, (0, "AA"), (0, "AA"), ["AA"])], end=26))
+    print("day 16: ", path_finder(data, distances, (q := [(0, 0, (0, "AA"), (0, "AA"), ["AA"])])[:], part1=True, end=30), path_finder(data, distances, q[:], end=26))
