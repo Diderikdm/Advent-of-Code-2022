bbd6056bbe074836e45f47885d604b4466e50f24
Diderik <diderikdm@gmail.com>
2022-12-20 12:07:42 +0100

    day 20

--- day_20_both_parts.py
+++ day_20_both_parts.py
@@ -1,11 +1,10 @@
-def find(data, shuffle, y, z):
-    for _ in range(z):
-        for x in data:
-            shuffle.insert(((i := shuffle.index(x)) + x[1]) % y, shuffle.pop(i))
+def find(data, shuffle, y):
+    for x in data:
+        shuffle.insert(((i := shuffle.index(x)) + x[1]) % y, shuffle.pop(i))
     zero = next((e for e, x in enumerate(shuffle) if x[1] == 0))
     return sum(shuffle[(zero + (x * 1000)) % (y + 1)][1] for x in [1, 2, 3])
-    
+
 with open("day_20.txt", "r") as file:
     p1 = list(enumerate(int(x) for x in file.read().splitlines()))
     p2 = [(x[0], x[1] * 811589153) for x in p1]
-    print("day 20 :", find(p1, p1[:], len(p1) - 1, 1), find(p2, p2[:], len(p2) - 1, 10))
+    print("day 20 :", find(p1, p1[:], len(p1) - 1), find(p2 * 10, p2[:], len(p2) - 1))
